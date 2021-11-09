9205f35f932bc6411887bf224ce95926f24400e0
EloiGry <eloi.grychta@gmail.com>
2021-11-09 14:00:37 +0100

    victory

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import onePiece from './one-piece.json'
 import './App.css';
 import Counter from './components/Counter';
+import Victory from './components/Victory';
 
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+        counterPlayer : 0,
+        counterComputer : 0
+    }
+    this.handleCounterPlayer = this.handleCounterPlayer.bind(this)
+    this.handleCounterComputer = this.handleCounterComputer.bind(this)
+  }
+
+  handleCounterPlayer () {
+    // if player win
+    this.setState ({ counterPlayer : this.state.counterPlayer + 1})
+}
+
+handleCounterComputer () {
+    // if computer win
+    this.setState ({ counterComputer : this.state.counterComputer + 1})
+}
+
 
   render() {
-    console.log(onePiece);
+    const {counterPlayer, counterComputer} = this.state
+    const result = counterPlayer + counterComputer < 21
+    
     return (
-      <div>
-        <h1>MEMORY</h1>
-        <Counter />
-      </div>
+      <>
+        { result ? (
+            <>
+              <h1> MEMORY </h1>
+              <Counter counterPlayer={counterPlayer} counterComputer={counterComputer}/>
+            </>
+          ) : (
+            <Victory counterPlayer={counterPlayer} counterComputer={counterComputer} />
+          )
+        }
+        
+
+      </>
       
     );
   }
