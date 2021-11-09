c822dea782ad03a29b184100e3f1f38764ecd271
KevinJeanMarie <kjeanmarie01@gmail.com>
2021-11-09 10:29:19 +0100

    button restart

---
+++ src/components/Restart.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Restart extends Component {
+    render() {
+        return (
+            <div>
+                <div className = "Restart-button"> 
+                <button>Restart</button>  
+            </div>
+            </div>
+        );
+    }
+}
+
+export default Restart;
