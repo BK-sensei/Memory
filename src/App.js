56dea15028444a3aa2d88a41de76fe10837d7b7d
daoraCode <severin.mboukou@gmail.com>
2021-11-09 15:23:21 +0100

    Rules component Updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-
 import './App.css';
-
 import React, { Component } from 'react';
+import Rules from './components/Rules';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
+  constructor () {
+    super()
+
+    // initial state data
+    this.state = {
+      showPopup: false
+    };
+
+    // bindings
+    this.togglePopupRules = this.togglePopupRules.bind(this);
+  }
+
+  // method rules popup
+  togglePopupRules() {
+    this.setState({ showPopup: !this.state.showPopup });
+  }
+
+  
+
+
   render() {
     return (
       <div>
-        <h1>MEMORY</h1>
+        <button
+
+          onClick={this.togglePopupRules.bind(this)}
+          type="button"
+          className="btn btn-primary">Règles du jeu</button>
+
+        {this.state.showPopup ?
+
+          <Rules
+            closePopup={this.togglePopupRules.bind(this)} />
+          : null
+        }
       </div>
     );
   }
