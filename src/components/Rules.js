3101f530f8b83e9d9608c205f452daa32d830f7f
becca <rebecca.kaci@gmail.com>
2021-11-12 16:01:16 +0100

    corrections bug rules

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -5,19 +5,21 @@ class Rules extends Component {
     render() {
         return (
             <div className="box modal">
-                <h1>Règles du jeu</h1>
+                <div className="d-flex justify-content-between align-items-center">
+                    <h1 class="modal-title" id="exampleModalLabel">Règles du jeu</h1>
+                    <button onClick={this.props.onClick} type="button" className="btn btn-danger my-5 py-2">Fermer</button>
+                </div>
                 <br />
                 <p>Choisissez une carte sur laquelle vous allez cliquer.</p>
-                <br />
+                {/* <br /> */}
                 <p>
                     Mémorisez le personnage de la carte et si la carte suivante choisie correspond au même personnage que la précédente
-                    vous gagnez un point. Si vous ne trouvez pas le personnage en deux cartes, alors ce sera au tour de l'adversaire de jouer.
-                </p>
-                <br />
+                    vous gagnez un point.</p>
+                <p>Si vous ne trouvez pas le personnage en deux cartes, alors ce sera au tour de l'adversaire de jouer.</p>
+                {/* <br /> */}
                 <p>La partie sera terminée lorsque toutes les cartes auront été trouvées en doubles.</p>
-                <br />
+                {/* <br /> */}
                 <p>Le joueur ayant trouvé le plus de doublons sera le vainqueur de la partie.</p>
-                <button onClick={this.props.onClick} type="button" className="btn btn-danger my-5 py-2">Fermer</button>
             </div>
         );
     }
